2745b87b7d0f26020fc9587b1973e7a3eb20c09b
Junhao Xie <118238091+ReidJunhaoXie@users.noreply.github.com>
2026-02-07 14:48:04 +0000

    commit 0207

--- tb/02.v
+++ tb/02.v
@@ -1,9 +1,18 @@
 `timescale 1ns / 1ps
 
 module simple_wire_02_tp();
+    logic clk ;
 
     logic tb_in;     // wire or reg --> logic
     logic tb_out;     
+
+    initial begin
+        clk = 0 ;
+        forever #5 clk = ~clk ;
+    end
+    always@(posedge clk) begin
+       $display("Time: %0t | Input: %b | Output: %b", $time, tb_in, tb_out) ; 
+    end
 
     simple_wire_02 dut(
         .in(tb_in),
@@ -15,7 +24,7 @@ module simple_wire_02_tp();
         $display("Starting Testbench: simple_wire");
         $display("--------------------------------------------------");
 
-        drive_and_check(1'b0);
+        drive_and_check(1'b0);  // task --> duplicated function
         drive_and_check(1'b1);
         drive_and_check(1'bx);
 
@@ -28,7 +37,8 @@ module simple_wire_02_tp();
     task drive_and_check(input logic val);   // task --> function
         begin
             tb_in = val;
-            #10; // delay 10 time unit
+            repeat(1) @(posedge clk) ;
+            #1 ; // delay 10 time unit
             if (tb_out === val) begin   // "===" --> case Euality
                 $display("[PASS] Input: %b, Output: %b", tb_in, tb_out);
             end else begin
