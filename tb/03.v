9511504fb7c8d10590e36347881c0682b7b17335
Junhao Xie <118238091+ReidJunhaoXie@users.noreply.github.com>
2026-02-07 15:56:31 +0000

    03 commit 2nd

--- tb/03.v
+++ tb/03.v
@@ -5,10 +5,14 @@ module tb_n_wire_03();
     logic a, b, c;           // Inputs
     logic w, x, y, z;        // Outputs
     
+    logic clk ;
     logic [3:0] expected_val;   // 正確答案
     logic [3:0] actual_val;     // 實際答案
-
-    n_wire_03 u_dut (
+    initial begin
+        clk = 0 ;
+        forever #5 clk = ~clk ;
+    end
+        n_wire_03 u_dut (
         .a(a), .b(b), .c(c),
         .w(w), .x(x), .y(y), .z(z)
     );                      // intantiate module
@@ -24,6 +28,7 @@ module tb_n_wire_03();
         // 瓊舉所有輸入組合 (2^3 = 8)
         for (int i = 0; i < 8; i++) begin // 宣告 32bit array i
             {a, b, c} = i[2:0];
+            repeat(1) @(posedge clk) ;
             #1; // delay 1 time unit 
             
             // 準備正確答案 
@@ -36,6 +41,7 @@ module tb_n_wire_03();
             end else begin
                 $display("%5tps | %b %b %b | %b %b %b %b | FAIL (Exp: %b)", 
                          $time, a, b, c, w, x, y, z, expected_val);
+                         $fatal ;
             end
         end
         $display("--------------------------------------------");
